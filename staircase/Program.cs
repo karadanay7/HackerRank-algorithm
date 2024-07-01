c4b4fa0e664882810ec9958e3648204a1e140f96
karadanay7 <107073245+karadanay7@users.noreply.github.com>
2024-07-01 15:07:33 +0300

    Add files via upload
    staircase solution added

---
+++ staircase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+       
+       for (int i = 1; i <= n; i++)
+        {
+            // Create a string with (n - i) spaces and i '#' characters
+            string line = new string(' ', n - i) + new string('#', i);
+            Console.WriteLine(line);
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
