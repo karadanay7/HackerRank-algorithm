b3d96548325c18bf0d28c31632b18df8c72236b8
karadanay7 <107073245+karadanay7@users.noreply.github.com>
2024-07-08 17:19:26 +0300

    minimax sum of array problem aolution added

---
+++ minimax-sum-of-array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+         arr.Sort();
+      long minSum =0;
+    for (int i=0 ; i<4; i++)
+      {
+     minSum += arr[i];
+         }
+      long maxSum = 0;
+    for (int i = arr.Count - 4; i < arr.Count; i++)
+
+        {
+     maxSum += arr[i];
+         }
+ Console.WriteLine($"{minSum} {maxSum}");
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
